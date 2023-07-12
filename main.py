250978cfb1b7603d1e0f2bb371b564ac0efa5d6d
dennism <transformes26k2@gmail.com>
2023-07-12 12:24:05 +0300

    Day 27: Access an email with news titles and descriptions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "73d67d6cfe6e4c3aa0094b9555a343ce"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
@@ -12,6 +13,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description.
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+
+body = body.encode('utf-8')
+send_email(message=body)
