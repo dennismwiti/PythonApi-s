cf1069e9bdd836d744efc91681ba1157077f1909
dennism <transformes26k2@gmail.com>
2023-07-13 07:30:01 +0300

    Day 28: Add links, specify language, limit news, add subject, control category.

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ content = request.json()
 
 # Access the article titles and description.
 body = ""
-for article in content['articles'][:20]:
-    if article['title'] is not None:
-        body = "Subject: Today's News" + "\n" + body + article['title'] + "\n" \
-               + article['description'] + "\n" \
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = "Subject: Today's News" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] + "\n" \
                + article["url"] + 2*"\n"
 
 body = body.encode('utf-8')
